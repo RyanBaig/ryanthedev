70e006a95e3b4bedbd8075de370a263292674474
RyanGamingYT <rg373429@gmail.com>
2023-10-04 13:12:17 +0500

    probably did something

--- api/categorysearch.js
+++ api/categorysearch.js
@@ -1,10 +1,21 @@
+import repoBadges from "../scripts/arrays.js"; // Import the 'repoBadges' array
+
 module.exports = (req, res) => {
-    const arrays = require("https://ryanthedev.vercel.app/scripts/arrays.js");
+  try {
     const searchTerm = req.query.q;
 
-    const filteredCategory = arrays.repoBadges.filter((badge) =>
+    // Ensure that 'repoBadges' is an array and then filter it
+    if (!Array.isArray(repoBadges)) {
+      throw new Error("Repo badges data is not an array.");
+    }
+
+    const filteredCategory = repoBadges.filter((badge) =>
       badge.category.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     res.json(filteredCategory);
-}
+  } catch (error) {
+    console.error("Error in serverless function:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
