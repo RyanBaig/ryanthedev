9024718d9a39b9034964d86e78c6eb547fb30bee
RyanGamingYT <rg373429@gmail.com>
2023-10-04 07:39:47 +0500

    lets see

--- backend/badgegenerator/categorysearch.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, res) => {
-  const searchTerm = req.query.q;
-
-  const filteredCategory = arrays.repoBadges.filter((badge) =>
-    badge.category.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  res.status(200).json(filteredCategory);
-};
