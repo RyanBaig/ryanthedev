84233326319901addaee7e37628f6c117f77d72a
RyanGamingYT <rg373429@gmail.com>
2023-10-04 14:10:35 +0500

    enabled CORS

--- api/badgegenerator/categorysearch.js
+++ api/badgegenerator/categorysearch.js
@@ -1,21 +1,32 @@
 import repoBadges from "../../scripts/arrays.js"; // Import the 'repoBadges' array
 
-module.exports = (req, res) => {
-  try {
-    const searchTerm = req.query.q;
+function categorySearch() {
+  (req, res) => {
+    try {
+      const searchTerm = req.query.q;
 
-    // Ensure that 'repoBadges' is an array and then filter it
-    if (!Array.isArray(repoBadges)) {
-      throw new Error("Repo badges data is not an array.");
+      // Ensure that 'repoBadges' is an array and then filter it
+      if (!Array.isArray(repoBadges)) {
+        throw new Error("Repo badges data is not an array.");
+      }
+
+      const filteredCategory = repoBadges.filter((badge) =>
+        badge.category.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      // Ensure that CORS is enabled
+      res.setHeader("Access-Control-Allow-Credentials", true);
+      res.setHeader("Access-Control-Allow-Origin", "*");
+
+      res.json(filteredCategory);
+    } catch (error) {
+      console.error("Error in serverless function:", error);
+      res
+        .status(500)
+        .json({
+          message:
+            "Please provide the 'q' query paramaters for searching the badges' categories!",
+        });
     }
-
-    const filteredCategory = repoBadges.filter((badge) =>
-      badge.category.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-    res.json(filteredCategory);
-  } catch (error) {
-    console.error("Error in serverless function:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
+  };
+}
+export default categorySearch;
